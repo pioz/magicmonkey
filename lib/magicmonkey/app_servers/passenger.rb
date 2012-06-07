03d95c0cf61636a126ad2cee34466a7d3a702131
Enrico Pilotto <enrico@megiston.it>
2012-06-07 15:46:53 +0200

    use bundle exec to run passenger and thin

--- lib/magicmonkey/app_servers/passenger.rb
+++ lib/magicmonkey/app_servers/passenger.rb
@@ -1,11 +1,11 @@
 module Passenger
 
   def self.start(args = {})
-    return "passenger start -e production -p #{args[:port]} #{args[:app_server_options]} -d"
+    return "bundle exec passenger start -e production -p #{args[:port]} #{args[:app_server_options]} -d"
   end
 
   def self.stop(args = {})
-    return "passenger stop -p #{args[:port]}"
+    return "bundle exec passenger stop -p #{args[:port]}"
   end
 
   def self.restart(args = {})
--- lib/magicmonkey/app_servers/thin.rb
+++ lib/magicmonkey/app_servers/thin.rb
@@ -1,11 +1,11 @@
 module Thin
 
   def self.start(args = {})
-    return "thin start -e production -p #{args[:port]} #{args[:app_server_options]} -d"
+    return "bundle exec thin start -e production -p #{args[:port]} #{args[:app_server_options]} -d"
   end
 
   def self.stop(args = {})
-    return "thin stop -p #{args[:port]}"
+    return "bundle exec thin stop -p #{args[:port]}"
   end
 
   def self.restart(args = {})
